53e4ef238fff467a6040c161e444881989c6ddc9
Maksym Hromyk <Maks>
2023-11-28 20:13:59 +0100

    initial commit

---
+++ src/main/java/mh/springboot/webplay/WebPlayApplication.java
@@ -0,0 +1,13 @@
+package mh.springboot.webplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPlayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebPlayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mh/springboot/webplay/WebPlayApplicationTests.java
@@ -0,0 +1,13 @@
+package mh.springboot.webplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebPlayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
